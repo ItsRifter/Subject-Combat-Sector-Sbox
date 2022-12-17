1c5aca75f9cf66c8217197fa50c188bd141a53ca
Cruton45 <ClaytonStout1@gmail.com>
2022-12-17 18:28:56 -0500

    Added ConVars
    Added convars: StartingPoints, TeamAmount, WinsNeeded.

---
+++ code/Gameplay/Game.ConVars.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Sandbox;
+
+namespace SCS;
+
+public partial class SCSGame
+{
+	[ConVar.Replicated]
+	public static int StartingPoints { get; set; } = 4;
+	[ConVar.Replicated]
+	public static int TeamAmount { get; set; } = 2;
+	[ConVar.Replicated]
+	public static int WinsNeeded { get; set; } = 2;
+}
+
